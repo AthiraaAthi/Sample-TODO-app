3f264bcd32fa804a3de3916bf919a84ab7691331
AthiraaAthi <athiraa2612@gmail.com>
2023-11-14 08:49:31 +0530

    created list of colors to apply

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,161 +1,222 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_15_/model/note_model/note_model.dart';
+import 'package:flutter_application_15_/utils/color_constant/color_constant.dart';
 import 'package:flutter_application_15_/view/home_screen/homescreen_widget/homescreen_widget.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
-
-
 
   @override
   State<Homescreen> createState() => _HomescreenState();
 }
 
 class _HomescreenState extends State<Homescreen> {
-  final nameController =TextEditingController();
-  final desController=TextEditingController();
-  final dateController=TextEditingController();
-  List<NoteModel>myNoteList=[ /////seperate model class
+  final nameController = TextEditingController();
+  final desController = TextEditingController();
+  final dateController = TextEditingController();
+  List<NoteModel> myNoteList = [
+    /////seperate model class
     NoteModel(title: "About My day", date: "jan2", description: "Not So Good"),
-     
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("ToDo"),
+        title: Text("ToDo"),
         backgroundColor: Color.fromARGB(255, 8, 227, 125),
-         ),
-       body: Padding(
-         padding: const EdgeInsets.all(20),
-         child: ListView.builder(
-            itemCount:myNoteList.length ,
-            itemBuilder: (context, index) => SizedBox(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: myNoteList.length,
+          itemBuilder: (context, index) => SizedBox(
               height: 200,
               width: 300,
-              child: HomeScreenWidget(//created widget
-              onDeletetap: () {
-                myNoteList.removeAt(index);//for deleting it from the widget declaration and here
-                setState(() {
-                  
-                });
-              },
-                title:myNoteList[index].title,
+              child: HomeScreenWidget(
+                //created widget
+                onDeletetap: () {
+                  myNoteList.removeAt(
+                      index); //for deleting it from the widget declaration and here
+                  setState(() {});
+                },
+                title: myNoteList[index].title,
                 description: myNoteList[index].description,
                 date: myNoteList[index].date,
-              )
-            ),
-            ),
-       ),
-       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showModalBottomSheet(context: context, builder:(context) => Container(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),color:Color(0xffEBE3D5)
-                      ) ,
-                      child: TextField(
-                        controller: nameController,
-                        
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          
-                          hintText: "Title"
-                        ),
-                        
-                      ),
-                      
-                    ),
-                  ),
-                  SizedBox(width: 20,),
-                  Container(
-                  padding: EdgeInsets.symmetric(horizontal: 50,vertical: 50),
-                    decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),color:Color(0xffEBE3D5)
-                      ) ,
-                      child: TextField(
-                        controller: desController,
-                        
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          
-                          hintText: "Description"
-                        ),
-                        
-                      ),
-            
-                  ),
-
-                  SizedBox(height: 20,),
-                  Container(
-                  
-                    decoration:BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),color:Color(0xffEBE3D5)
-                      ) ,
-                      child: TextField(
-                        controller: dateController,
-                        
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          
-                          hintText: "date"
-                        ),
-                        
-                      ),
-            
-                  ),
-                  SizedBox(height: 10,),
-                  Row(
+              )),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) =>
+                StatefulBuilder(builder: (context, insetState) {
+              //insetState is custom name
+              return Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
                     children: [
-                      InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 107, 230, 111),borderRadius: BorderRadius.circular(20)),)),
-                      SizedBox(width: 15,),
-                       InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 243, 173, 62),borderRadius: BorderRadius.circular(20)),)),
-                       SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 123, 218, 237),borderRadius: BorderRadius.circular(20)),)),
-                         SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 236, 146, 202),borderRadius: BorderRadius.circular(20)),)),
-                         SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 240, 96, 96),borderRadius: BorderRadius.circular(20)),)),
-                         SizedBox(width: 15,),
-                        InkWell(child: Container(height: 50,width: 50,decoration: BoxDecoration(color: Color.fromARGB(255, 172, 76, 227),borderRadius: BorderRadius.circular(20)),)),
-                        
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color(0xffEBE3D5)),
+                          child: TextField(
+                            controller: nameController,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Title"),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffEBE3D5)),
+                        child: TextField(
+                          controller: desController,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Description"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffEBE3D5)),
+                        child: TextField(
+                          controller: dateController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(), hintText: "date"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children:
+                              //list.generate use chyth
+                              List.generate(
+                            ColorConstant.PrimaryColor.length,
+                            (index) => StatefulBuilder(
+                              builder: (context, insetState) => InkWell(
+                                onTap: () {
+                                  insetState(() {});
+                                },
+                                child: Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                    color: ColorConstant.PrimaryColor[index],
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+
+                          // InkWell(
+                          //     child: Container(
+                          //   height: 50,
+                          //   width: 50,
+                          //   decoration: BoxDecoration(
+                          //       color: Color.fromARGB(255, 107, 230, 111),
+                          //       borderRadius: BorderRadius.circular(20)),
+                          // )),
+                          // SizedBox(
+                          //   width: 15,
+                          // ),
+                          // InkWell(
+                          //     child: Container(
+                          //   height: 50,
+                          //   width: 50,
+                          //   decoration: BoxDecoration(
+                          //       color: Color.fromARGB(255, 243, 173, 62),
+                          //       borderRadius: BorderRadius.circular(20)),
+                          // )),
+                          // SizedBox(
+                          //   width: 15,
+                          // ),
+                          // InkWell(
+                          //     child: Container(
+                          //   height: 50,
+                          //   width: 50,
+                          //   decoration: BoxDecoration(
+                          //       color: Color.fromARGB(255, 123, 218, 237),
+                          //       borderRadius: BorderRadius.circular(20)),
+                          // )),
+                          // SizedBox(
+                          //   width: 15,
+                          // ),
+                          // InkWell(
+                          //     child: Container(
+                          //   height: 50,
+                          //   width: 50,
+                          //   decoration: BoxDecoration(
+                          //       color: Color.fromARGB(255, 236, 146, 202),
+                          //       borderRadius: BorderRadius.circular(20)),
+                          // )),
+                          // SizedBox(
+                          //   width: 15,
+                          // ),
+                          // InkWell(
+                          //     child: Container(
+                          //   height: 50,
+                          //   width: 50,
+                          //   decoration: BoxDecoration(
+                          //       color: Color.fromARGB(255, 240, 96, 96),
+                          //       borderRadius: BorderRadius.circular(20)),
+                          // )),
+                          // SizedBox(
+                          //   width: 15,
+                          // ),
+                          // InkWell(
+                          //     child: Container(
+                          //   height: 50,
+                          //   width: 50,
+                          //   decoration: BoxDecoration(
+                          //       color: Color.fromARGB(255, 172, 76, 227),
+                          //       borderRadius: BorderRadius.circular(20)),
+                          // )),
+
+                          ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            myNoteList.add(NoteModel(
+                                title: nameController.text,
+                                date: dateController.text,
+                                description: desController.text));
+                            setState(() {});
+                            nameController.clear();
+                            desController.clear();
+                            dateController.clear();
+                            Navigator.pop(context);
+                          },
+                          child: Text("Save"))
                     ],
                   ),
-                  SizedBox(height: 8,),
-                 ElevatedButton(onPressed: (){
-                 myNoteList.add(
-                  NoteModel(
-                    title:nameController.text , date:dateController.text , description: desController.text
-                    )
-                 );
-                 setState(() {
-                   
-                 });
-                 nameController.clear();
-                 desController.clear();
-                 dateController.clear();   
-                 Navigator.pop(context);             
-                 },
-                  child: Text("Save"))
-                  
-                 
-            
-                ],
-              ),
-            ),
-
-           
-
-          ),
-           );
-        },child: Icon(Icons.add),),
-        );
+                ),
+              );
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
